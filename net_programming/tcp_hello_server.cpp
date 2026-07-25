8958263afaf0882a6a63290bf8a42d3d9f796cf2
Rrrayy <3111937934@qq.com>
2026-07-25 22:53:59 +0800

    实现TCP hello服务器，建立socket编程基础骨架

---
+++ net_programming/tcp_hello_server.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstring>
+#include<unistd.h>
+#include<cstdlib>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+int main(){
+	
+	//"创建监听"
+	int listen_fd = socket(AF_INET,SOCK_STREAM,0);
+	if(listen_fd<0){
+		perror("error listen");
+		exit(1);
+	}
+	
+	//设置地址复用
+	int opt=1;
+	setsockopt(listen_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+	
+	//绑定地址和端口
+	sockaddr_in addr{};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8888);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	if(bind(listen_fd , (sockaddr*)&addr , sizeof(addr))<0){
+		perror("error bind");
+		close(listen_fd);
+		exit(1);
+	}
+	
+	//开始监听
+	if(listen(listen_fd,128)<0){
+		perror("error listen");
+		close(listen_fd);
+		exit(1);
+	}
+
+	std::cout << "[服务器] 监听 127.0.0.1:8888" << std::endl;
+	
+	//等待客户连接
+	sockaddr_in client_addr{};
+	socklen_t client_len = sizeof(client_addr);
+	int conn_fd = accept(listen_fd , (sockaddr*)&client_addr,&client_len);
+	if(conn_fd<0){
+		perror("error accept");
+		close(listen_fd);
+		exit(1);	
+	}
+	std::cout << "[服务器] 客户端已连接！" << std::endl;
+	
+	//发送消息
+	const char* msg="hello";
+	write(conn_fd,msg,strlen(msg));
+	std::cout << "[服务器] 已发送: " << msg << std::endl;
+	
+	//关闭连接
+	close(conn_fd);
+	close(listen_fd);
+	std::cout << "[服务器] 连接已关闭" << std::endl;
+	return 0;
+}
